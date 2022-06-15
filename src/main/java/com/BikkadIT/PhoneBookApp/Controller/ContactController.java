a867fd8b8e93524a1fec96a56f9344b86be39449
sagarbobade1602 <sagar.bobade786@gmail.com>
2022-06-16 01:16:21 +0530

    PHON-11:create method in controller class for inserting contact detail into server

--- src/main/java/com/BikkadIT/PhoneBookApp/Controller/ContactController.java
+++ src/main/java/com/BikkadIT/PhoneBookApp/Controller/ContactController.java
@@ -1,9 +1,35 @@
 package com.BikkadIT.PhoneBookApp.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.BikkadIT.PhoneBookApp.Model.Contact;
+import com.BikkadIT.PhoneBookApp.Service.ContactServiceI;
 
 @RestController
 public class ContactController {
 
+	@Autowired
+	private ContactServiceI contactServiceI;
 	
+	@PostMapping(value="/saveContact",consumes="APPLICATION/JSON")
+	public ResponseEntity<String> saveContact(@RequestBody Contact contact){
+		
+		boolean save=contactServiceI.saveContact(contact);
+		
+		if(save == true) {
+			String msg="Contact saved sucessfully";
+			return new ResponseEntity<String>(msg,HttpStatus.OK);
+		}else {
+			String msg="Contact not saved sucessfully";
+			return new ResponseEntity<String>(msg,HttpStatus.BAD_REQUEST);
+		}
+		
+		
+	
+}
 }
--- src/main/java/com/BikkadIT/PhoneBookApp/Service/ContactServiceImpl.java
+++ src/main/java/com/BikkadIT/PhoneBookApp/Service/ContactServiceImpl.java
@@ -1,8 +1,17 @@
 package com.BikkadIT.PhoneBookApp.Service;
 
-import org.springframework.stereotype.Service;
 
-@Service
-public class ContactServiceImpl {
+import org.springframework.web.bind.annotation.RestController;
+
+import com.BikkadIT.PhoneBookApp.Model.Contact;
+
+@RestController
+public class ContactServiceImpl implements ContactServiceI{
+
+	@Override
+	public boolean saveContact(Contact contact) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
